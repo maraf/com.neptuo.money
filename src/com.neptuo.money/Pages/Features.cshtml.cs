17947f0478dfa1f6c7757a8270a381c96abba2fe
Marek Fišera <mara@neptuo.com>
2019-03-18 14:49:08 +0100

    #4 - Add empty Features page.

---
+++ src/com.neptuo.money/Pages/Features.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace com.neptuo.money.Pages
+{
+    public class FeaturesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
